36f83158446b83bf20394cd43518c7a7a8ab8594
Shaun <Shaun@Shauns-MacBook-Pro.local>
2021-08-18 12:52:32 -0600

    add map component and insert the mapbox keys

---
+++ components/Map.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import ReactMapGL from 'react-map-gl';
+
+const Map = () => {
+  return (
+    <ReactMapGL
+      mapStyle='mapbox://styles/shaunp/ckshuat1jj76s17lyq18p8d07'
+      mapboxApiAccessToken=''
+    >
+      
+    </ReactMapGL>
+  )
+}
+
+export default Map
