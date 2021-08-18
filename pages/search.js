f08ed4978edd1ab5a117f70c9cf834e733df737a
Shaun <Shaun@Shauns-MacBook-Pro.local>
2021-08-17 19:41:27 -0600

    create search endpoint and add header and footer component and add buttons inside the main section

---
+++ pages/search.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import Header from '../components/Header';
+import Footer from '../components/Footer';
+
+const Search = () => {
+  return (
+    <div>
+      <Header />
+
+      <main className='flex'>
+        <section>
+          <p className='text-xs'>300+ Stays for 5 number of guests</p>
+          <h1 className='text-3xl font-semibold mt-2 mb-6'>Stays in Mars</h1>
+
+          <div className=''>
+            <p className='button'>Cancellation Flexibility</p>
+            <p className='button'>Type of Place</p>
+            <p className='button'>Price</p>
+            <p className='button'>Rooms and Beds</p>
+            <p className='button'>More filters</p>
+          </div>
+        </section>
+      </main>
+
+      <Footer />
+    </div>
+  );
+};
+
+export default Search;
